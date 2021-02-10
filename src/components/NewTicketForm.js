7de83425abeb74f2c720244879e7f5ce6baac241
Shannon Grantski <grantski@pm.me>
2021-02-10 09:38:50 -0800

    add form for creating new ticket
    Co-authored-by: Ellie Lambert <ebezjian@gmail.com>

--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -1,11 +1,31 @@
 import React from "react";
+import {v4} from 'uuid';
 
-function NewTicketForm(props){
+function NewTicketForm(){
   return (
     <>
-      <h3>This is a form!</h3>
+      <form onSubmit={handleNewTicketFormSubmission}>
+        <input
+          type='text'
+          name='names'
+          placeholder='Pair Names' />
+        <input
+          type='text'
+          name='location'
+          placeholder='Location' />
+        <textarea
+          name='issue'
+          placeholder='Describe your issue' />
+        <button type ='Submit'>Help!</button>
+      </form>
     </>
   );
-}
 
+  function handleNewTicketFormSubmission(event){
+    event.preventDefault();
+    console.log(event.target.names.value);
+    console.log(event.target.location.value);
+    console.log(event.target.issue.value);
+  }
+}
 export default NewTicketForm;

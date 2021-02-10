da16fc8c4f02916729709dda61e2a88497bab108
Shannon Grantski <grantski@pm.me>
2021-02-10 10:06:55 -0800

    pass in proptypes
    Co-authored-by: Ellie Lambert <ebezjian@gmail.com>

--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -1,7 +1,8 @@
 import React from "react";
 import {v4} from 'uuid';
+import PropTypes from "prop-types";
 
-function NewTicketForm(){
+function NewTicketForm(props){
   return (
     <>
       <form onSubmit={handleNewTicketFormSubmission}>
@@ -16,16 +17,24 @@ function NewTicketForm(){
         <textarea
           name='issue'
           placeholder='Describe your issue' />
-        <button type ='Submit'>Help!</button>
+        <button type ='Submit'>Submit ticket!</button>
       </form>
     </>
   );
 
   function handleNewTicketFormSubmission(event){
     event.preventDefault();
-    console.log(event.target.names.value);
-    console.log(event.target.location.value);
-    console.log(event.target.issue.value);
+    props.onNewTicketCreation({
+      names: event.target.names.value,
+      location: event.target.location.value,
+      issue: event.target.issue.value,
+      id: v4()
+    });
   }
 }
+
+NewTicketForm.propTypes = {
+  onNewTicketCreation: PropTypes.func
+};
+
 export default NewTicketForm;

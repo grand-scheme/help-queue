f10a7100d0095dd993d6bce93625364407419c6f
Shannon Grantski <grantski@pm.me>
2021-02-10 13:38:23 -0800

    move Ticket into TicketControl-Children
    Co-authored-by: Ellie Lambert <ebezjian@gmail.com>

--- src/components/App-Children/TicketControl-Children/TicketList-Children/Ticket.js
+++ src/components/App-Children/TicketControl-Children/TicketList-Children/Ticket.js
@@ -2,14 +2,12 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function Ticket(props){
-  const name = "Thato";
-  const name2 = "Haley";
   return(
-    <React.Fragment>
+    <>
       <h3>{props.location} - {props.names}</h3>
       <p><em>{props.issue}</em></p>
       <hr />
-    </React.Fragment>
+    </>
   );
 }
 

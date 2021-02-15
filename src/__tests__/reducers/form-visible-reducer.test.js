e54cedfc5f7437d80e99dcf457e4dedf491e6ca5
Shannon Grantski <grantski@pm.me>
2021-02-15 14:24:20 -0800

    begin testing for form visible reducer
    Co-authored-by: Jeff Chiu <jeffchiudev@gmail.com>

---
+++ src/__tests__/reducers/form-visible-reducer.test.js
@@ -0,0 +1,8 @@
+import formVisibleReducer from '../../reducers/form-visible-reducer';
+
+describe ("formVisibleReduce", () => {
+
+  test('Should return default state if no action type is recognized', () => {
+    expect(formVisibleReuducer(false, { type: null })).toEqual(false);
+  });
+});

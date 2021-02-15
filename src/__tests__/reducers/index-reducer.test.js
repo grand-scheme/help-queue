3f9f2650ab4c1bacd65e341144b989d3687cadcf
Shannon Grantski <grantski@pm.me>
2021-02-15 14:54:01 -0800

    adjust import statements, pass test
    Co-authored-by: Jeff Chiu <jeffchiudev@gmail.com>

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -1,7 +1,7 @@
-import rootReducer from '../../reducers/index';
 import { createStore } from 'redux';
 import formVisibleReducer from '../../reducers/form-visible-reducer';
-import ticketListReducer from '../../reducer/ticket-list-reducer';
+import ticketListReducer from '../../reducers/ticket-list-reducer';
+import rootReducer from '../../reducers/index-reducer';
 
 let store = createStore(rootReducer);
 
@@ -20,5 +20,25 @@ describe ("rootReducer", () => {
 
   test('Check that initial state of formVisibleReducer matches root reducer', () => {
     expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null }));
+  });
+
+  test('Check that ADD_TICKET action works for ticketListReducer and root reducer', () => {
+    const action ={
+      type: 'ADD_TICKET',
+      names: 'Ryan & Aimen',
+      location: '4b',
+      issue: 'Redux action is not working correctly.',
+      id: 1
+    }
+    store.dispatch(action);
+    expect(store.getState().masterTicketList).toEqual(ticketListReducer(undefined, action));
+  });
+
+  test('Check that TOGGLE_FORM action works for formVisibleReducer and root reducer', () => {
+    const action = {
+      type: 'TOGGLE_FORM'
+    }
+    store.dispatch(action);
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, action));
   });
 });
